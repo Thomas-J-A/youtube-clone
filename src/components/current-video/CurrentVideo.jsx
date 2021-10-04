45aa1ac0eb08ba065cd6052c633a5ea072b804a5
Thomas Aspbury <tomaspbury@icloud.com>
2021-10-04 10:14:47 +0100

    Embed YouTube video

--- src/components/current-video/CurrentVideo.jsx
+++ src/components/current-video/CurrentVideo.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const CurrentVideo = () => (
-  <section class="current-video">
-    <p>Current Video</p>
+  <section className="current-video">
+    <div className="video-wrapper">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/xUrAqEzbe2I" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+    </div>
   </section>
 );
 
